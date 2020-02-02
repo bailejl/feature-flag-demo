9d5e3c60664207584820e62891f5afef5cd2af75
Joe Bailey <bailejl@yahoo.com>
2020-02-02 09:29:00 -0800

    Improved the maintenance flag and added the banner flag.

--- public/browser.js
+++ public/browser.js
@@ -3,8 +3,12 @@ const clientState = {};
 
 const flags = [
   {
-    name: 'maintenance',
+    name: 'perm-maintenance',
     handler: maintenanceHandler
+  },
+  {
+    name: 'perm-banner',
+    handler: bannerHandler
   }
 ];
 
@@ -12,7 +16,6 @@ function handleClientFeatureFlagsRsp(httpRequest, clientId){
   return function() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        console.log(`${clientId}: ${httpRequest.responseText}`);
         const flagData = JSON.parse(httpRequest.responseText);
         const f = flags.find((flag) => flag.name === flagData['flagName']);
         if (f) {
@@ -30,7 +33,7 @@ function handleClientRegistrationRsp(httpRequest, clientId) {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         getLatestClientFlags(clientId);
-        clientState[clientId]['intervalId'] = window.setInterval(getLatestClientFlags, 7000, clientId);
+        clientState[clientId]['intervalId'] = window.setInterval(getLatestClientFlags, 3000, clientId);
       } else {
         console.error('There was a problem with the request.');
       }
@@ -69,16 +72,18 @@ function loadSquares() {
   [...Array(20).keys()].forEach((val) => {
     const clientContainer = document.createElement("div");
     const topBar = document.createElement("div");
-    topBar.id = `topBar`;
     const bottomBar = document.createElement("div");
-    bottomBar.id = `bottomBar`;
     const clientId = val + 1;
     clientContainer.id = `container-${clientId}`;
+    topBar.id = `top-bar-${clientId}`;
+    bottomBar.id = `bottom-bar-${clientId}`;
     const textElement = document.createElement("p");
     textElement.id = `text`;
     const textNode = document.createTextNode(`${clientId}`);
 
     clientContainer.classList.add('client-container');
+    topBar.classList.add('top-bar');
+    bottomBar.classList.add('bottom-bar');
     textElement.appendChild(textNode);
     clientContainer.appendChild(topBar);
     clientContainer.appendChild(textElement);
@@ -99,5 +104,19 @@ function maintenanceHandler(clientId, flagData){
     document.getElementById(`container-${clientId}`).classList.add('maintenance-mode');
   } else {
     document.getElementById(`container-${clientId}`).classList.remove('maintenance-mode');
+  }
+}
+
+function bannerHandler(clientId, flagData){
+  console.log(`${clientId}: ${JSON.stringify(flagData)}`);
+  if (flagData.enabled && flagData.payload && flagData.payload.value) {
+    const msgObj = JSON.parse(flagData.payload.value);
+    document.getElementById(`bottom-bar-${clientId}`).classList.add('show-banner');
+    document.getElementById(`bottom-bar-${clientId}`).classList.add(`msg-severity-${msgObj['severity']}`);
+    document.getElementById(`bottom-bar-${clientId}`).innerText = msgObj['message'];
+  } else {
+    document.getElementById(`bottom-bar-${clientId}`).classList.remove('show-banner');
+    document.getElementById(`bottom-bar-${clientId}`).classList.remove(`msg-severity-low`, `msg-severity-medium`, `msg-severity-high`);
+    document.getElementById(`bottom-bar-${clientId}`).innerText = '';
   }
 }
